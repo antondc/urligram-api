02f9364d295734ba917ab82a0891adbe33f351d4
Antonio Díaz Correa <antonio.d.correa@gmail.com>
2020-07-12 12:22:38 +0300

    Refactor userList data

--- src/infrastructure/persistence/mySQL/sql/data/userList.sql
+++ src/infrastructure/persistence/mySQL/sql/data/userList.sql
@@ -13,23 +13,23 @@ INSERT INTO `user_list` (
 INSERT INTO `user_list` (
   `id`, `order`, `createdAt`, `updatedAt`, `list_id`, `userRole`, `user_id`
 ) VALUES (
-  3, 3, DEFAULT, DEFAULT, 3, "admin", 'b95274c9-3d26-4ce3-98b2-77dce5bd7aae'
+  3, 3, DEFAULT, DEFAULT, 2, "reader", 'b95274c9-3d26-4ce3-98b2-77dce5bd7aae'
 );
 
 INSERT INTO `user_list` (
   `id`, `order`, `createdAt`, `updatedAt`, `list_id`, `userRole`, `user_id`
 ) VALUES (
-  4, 4, DEFAULT, DEFAULT, 3, "editor", '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000'
+  4, 4, DEFAULT, DEFAULT, 2, "admin", 'e4e2bb46-c210-4a47-9e84-f45c789fcec1'
 );
 
 INSERT INTO `user_list` (
   `id`, `order`, `createdAt`, `updatedAt`, `list_id`, `userRole`, `user_id`
 ) VALUES (
-  5, 5, DEFAULT, DEFAULT, 3, "reader", 'e4e2bb46-c210-4a47-9e84-f45c789fcec1'
+  5, 5, DEFAULT, DEFAULT, 3, "admin", '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000'
 );
 
 INSERT INTO `user_list` (
   `id`, `order`, `createdAt`, `updatedAt`, `list_id`, `userRole`, `user_id`
 ) VALUES (
-  6, 6, DEFAULT, DEFAULT, 2, "reader", 'e4e2bb46-c210-4a47-9e84-f45c789fcec1'
+  6, 6, DEFAULT, DEFAULT, 4, "admin", 'e4e2bb46-c210-4a47-9e84-f45c789fcec1'
 );
