112e85ffc234b0b075821e6e0da8716f49d110d1
Antonio Díaz Correa <antonio.d.correa@gmail.com>
2020-12-20 19:05:21 +0300

    Add tags to Bookmarks

--- src/infrastructure/persistence/mySQL/sql/data/bookmarkTag.sql
+++ src/infrastructure/persistence/mySQL/sql/data/bookmarkTag.sql
@@ -13,47 +13,119 @@ INSERT INTO `bookmark_tag` (
 INSERT INTO `bookmark_tag` (
   `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
 ) VALUES (
-  2, DEFAULT, DEFAULT, 2, 1
+  2, DEFAULT, DEFAULT, 1, 3
 );
 
 INSERT INTO `bookmark_tag` (
   `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
 ) VALUES (
-  3, DEFAULT, DEFAULT, 2, 2
+  3, DEFAULT, DEFAULT, 1, 4
 );
 
 INSERT INTO `bookmark_tag` (
   `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
 ) VALUES (
-  4, DEFAULT, DEFAULT, 3, 1
+  4, DEFAULT, DEFAULT, 1, 5
 );
 
 INSERT INTO `bookmark_tag` (
   `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
 ) VALUES (
-  5, DEFAULT, DEFAULT, 3, 2
+  5, DEFAULT, DEFAULT, 1, 6
 );
 
 INSERT INTO `bookmark_tag` (
   `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
 ) VALUES (
-  6, DEFAULT, DEFAULT, 3, 4
+  6, DEFAULT, DEFAULT, 1, 7
 );
 
 INSERT INTO `bookmark_tag` (
   `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
 ) VALUES (
-  7, DEFAULT, DEFAULT, 4, 1
+  7, DEFAULT, DEFAULT, 2, 8
 );
 
 INSERT INTO `bookmark_tag` (
   `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
 ) VALUES (
-  8, DEFAULT, DEFAULT, 4, 2
+  8, DEFAULT, DEFAULT, 2, 1
 );
 
 INSERT INTO `bookmark_tag` (
   `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
 ) VALUES (
-  9, DEFAULT, DEFAULT, 4, 5
+  9, DEFAULT, DEFAULT, 2, 3
+);
+
+INSERT INTO `bookmark_tag` (
+  `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
+) VALUES (
+  10, DEFAULT, DEFAULT, 2, 5
+);
+
+INSERT INTO `bookmark_tag` (
+  `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
+) VALUES (
+  11, DEFAULT, DEFAULT, 2, 6
+);
+
+INSERT INTO `bookmark_tag` (
+  `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
+) VALUES (
+  12, DEFAULT, DEFAULT, 3, 1
+);
+
+INSERT INTO `bookmark_tag` (
+  `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
+) VALUES (
+  13, DEFAULT, DEFAULT, 3, 8
+);
+
+INSERT INTO `bookmark_tag` (
+  `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
+) VALUES (
+  14, DEFAULT, DEFAULT, 3, 3
+);
+
+INSERT INTO `bookmark_tag` (
+  `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
+) VALUES (
+  15, DEFAULT, DEFAULT, 3, 4
+);
+
+INSERT INTO `bookmark_tag` (
+  `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
+) VALUES (
+  16, DEFAULT, DEFAULT, 3, 5
+);
+
+INSERT INTO `bookmark_tag` (
+  `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
+) VALUES (
+  17, DEFAULT, DEFAULT, 3, 6
+);
+
+INSERT INTO `bookmark_tag` (
+  `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
+) VALUES (
+  18, DEFAULT, DEFAULT, 3, 7
+);
+
+INSERT INTO `bookmark_tag` (
+  `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
+) VALUES (
+  20, DEFAULT, DEFAULT, 4, 1
+);
+
+INSERT INTO `bookmark_tag` (
+  `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
+) VALUES (
+  21, DEFAULT, DEFAULT, 4, 2
+);
+
+INSERT INTO `bookmark_tag` (
+  `order`, `createdAt`, `updatedAt`, `bookmark_id`, `tag_id`
+) VALUES (
+  22, DEFAULT, DEFAULT, 4, 5
 );
