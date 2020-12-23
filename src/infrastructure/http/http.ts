a700f16ce0662b20a1e6500b3b806a1bd535effc
Antonio Díaz Correa <antonio.d.correa@gmail.com>
2020-12-23 11:46:27 +0100

    Revert "Modify CORS"
    This reverts commit e7306044a05c8a92bac367c6a9744b98692079cc.

--- src/infrastructure/http/http.ts
+++ src/infrastructure/http/http.ts
@@ -1,59 +1,57 @@
 import 'module-alias/register';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
 import http from 'http';
+import logger from 'morgan';
 
-// import { AuthMiddleware } from '@infrastructure/http/middlewares/AuthMiddleware';
-// import { ErrorHandlerMiddleware } from '@infrastructure/http/middlewares/ErrorHandlerMiddleware';
-// import { RouterV1 } from '@infrastructure/http/routesV1';
-import { PORT_SERVER } from '@shared/constants/env';
+import { AuthMiddleware } from '@infrastructure/http/middlewares/AuthMiddleware';
+import { ErrorHandlerMiddleware } from '@infrastructure/http/middlewares/ErrorHandlerMiddleware';
+import { RouterV1 } from '@infrastructure/http/routesV1';
+import { ENDPOINT_CLIENTS, PORT_SERVER } from '@shared/constants/env';
 
 const app = express();
 
 /* - - - - - - - - - - - Cors - - - - - - - - - - - - - - */
 
-// app.use(cors({ credentials: true, origin: ENDPOINT_CLIENTS }));
-// /* - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+app.use(cors({ credentials: true, origin: ENDPOINT_CLIENTS }));
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 
-// /* - - - - - - - - - - - Static - - - - - - - - - - - - - */
-// // Serving static files
-// app.use(express.static('media/docs'));
-// app.use(express.static('media/images'));
-// app.use('/media', express.static('media'));
-// /* - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+/* - - - - - - - - - - - Static - - - - - - - - - - - - - */
+// Serving static files
+app.use(express.static('media/docs'));
+app.use(express.static('media/images'));
+app.use('/media', express.static('media'));
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 
-// /* - - - - - - - - - - - Parsers - - - - - - - - - - - - -*/
-// // Parsing application/x-www-form-urlencoded:
-// app.use(bodyParser.urlencoded({ extended: false }));
-// // Parsing body
-// app.use(bodyParser.json());
-// // Parsing JSON
-// app.use(express.json());
-// // Parsing req.body
-// app.use(express.urlencoded({ extended: false }));
-// // Parsing cookies
-// app.use(cookieParser());
-// /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/* - - - - - - - - - - - Parsers - - - - - - - - - - - - -*/
+// Parsing application/x-www-form-urlencoded:
+app.use(bodyParser.urlencoded({ extended: false }));
+// Parsing body
+app.use(bodyParser.json());
+// Parsing JSON
+app.use(express.json());
+// Parsing req.body
+app.use(express.urlencoded({ extended: false }));
+// Parsing cookies
+app.use(cookieParser());
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-// /* - - - - - - - - - - - Loggers - - - - - - - - - - - - - */
-// app.use(logger('dev'));
-// /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-// /* - - - - - - - - - - - Routes - - - - - - - - - - - - - -*/
-// app.use('*', AuthMiddleware);
-// app.use('/api/v1/', RouterV1);
-// /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/* - - - - - - - - - - - Loggers - - - - - - - - - - - - - */
+app.use(logger('dev'));
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/* - - - - - - - - - - - Routes - - - - - - - - - - - - - -*/
+app.use('*', AuthMiddleware);
+app.use('/api/v1/', RouterV1);
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-// /* - - - - - - - - - - - Errors Handler - - - - - - - - - - - - - -*/
-// app.use('*', ErrorHandlerMiddleware);
-// /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/* - - - - - - - - - - - Errors Handler - - - - - - - - - - - - - -*/
+app.use('*', ErrorHandlerMiddleware);
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 /* - - - - - - - - - - - Server - - - - - - - - - - - - - -*/
 const server = http.createServer(app);
-
-app.get('*', (req, res) => {
-  console.log(req.headers);
-
-  return res.send(req.headers);
-});
 
 server.listen(PORT_SERVER);
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
