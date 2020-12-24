6d3d7513cdfe4f6f56259b2867feb8cd987dddab
Antonio Díaz Correa <antonio.d.correa@gmail.com>
2020-12-24 12:46:43 +0100

    Add type casting to MySQL driver

--- src/infrastructure/persistence/mySQL/services/MySQL.ts
+++ src/infrastructure/persistence/mySQL/services/MySQL.ts
@@ -12,7 +12,25 @@ export class MySQL {
     this.mySQL = mysql.createConnection({
       ...DATABASE_SETTINGS,
       multipleStatements,
+      typeCast: this.typeCast,
     });
+  }
+
+  typeCast(field, useDefaultTypeCasting) {
+    // We only want to cast TINY fields that have a single digit in them. If the field
+    // has more than one integer, then we cannot assume it is supposed to be a Boolean.
+    if (field.type === 'TINY' && field.length === 1) {
+      const bytes = field.buffer(); // Extract value
+
+      // If there are no bytes, we are at a NULL value
+      if (!bytes) return;
+      // Convert from ASCII to string, then to number and finally to boolean
+      const value = Boolean(Number(String.fromCharCode(bytes[0])));
+
+      return value;
+    }
+
+    return useDefaultTypeCasting();
   }
 
   query(sql: string, args?: unknown) {
