3afc88978c9452778c7c81b90b1ea57a0deffdb5
Antonio Díaz Correa <antonio.d.correa@gmail.com>
2021-01-23 12:24:27 +0300

    Add data lists

--- src/infrastructure/persistence/mySQL/sql/data/list.sql
+++ src/infrastructure/persistence/mySQL/sql/data/list.sql
@@ -6,4 +6,10 @@ INSERT INTO `list` (
   (3, 'Third list', 'Third list', false, '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000', DEFAULT, DEFAULT),
   (4, 'Fourth list', 'Fourth list', true, 'e4e2bb46-c210-4a47-9e84-f45c789fcec1', DEFAULT, DEFAULT),
   (5, 'Fifth list', 'Fifth list', true, 'e4e2bb46-c210-4a47-9e84-f45c789fcec1', DEFAULT, DEFAULT),
-  (6, 'Sixth list', 'Sixth list', true, 'e4e2bb46-c210-4a47-9e84-f45c789fcec1', DEFAULT, DEFAULT);
+  (6, 'Sixth list', 'Sixth list', true, 'e4e2bb46-c210-4a47-9e84-f45c789fcec1', DEFAULT, DEFAULT),
+  (7, 'Seventh list', 'Seventh list', false, 'b95274c9-3d26-4ce3-98b2-77dce5bd7aae', DEFAULT, DEFAULT),
+  (8, 'Eighth list', 'Eighthlist', true, 'e4e2bb46-c210-4a47-9e84-f45c789fcec1', DEFAULT, DEFAULT),
+  (9, 'Ninth list', 'Ninthlist', false, '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000', DEFAULT, DEFAULT),
+  (10, 'Tenth list', 'Tenth list', true, 'e4e2bb46-c210-4a47-9e84-f45c789fcec1', DEFAULT, DEFAULT),
+  (11, 'Eleventh list', 'Eleventh list', true, 'e4e2bb46-c210-4a47-9e84-f45c789fcec1', DEFAULT, DEFAULT),
+  (12, 'Twelfth list', 'Sixth Twelfthst', true, 'e4e2bb46-c210-4a47-9e84-f45c789fcec1', DEFAULT, DEFAULT);
