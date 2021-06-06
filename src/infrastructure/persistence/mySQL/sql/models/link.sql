fd7dfe7c718e01582e07dc206b52e6f96b386c1e
Antonio Díaz Correa <antonio.d.correa@gmail.com>
2021-06-07 00:50:31 +0300

    Update user fix

--- src/infrastructure/persistence/mySQL/sql/models/link.sql
+++ src/infrastructure/persistence/mySQL/sql/models/link.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `link` (
   `title` VARCHAR(255) NOT NULL,
   `path` VARCHAR(1000) NOT NULL,
   `description` TEXT DEFAULT NULL,
-  `favicon` VARCHAR(255) DEFAULT NULL,
+  `favicon` TEXT DEFAULT NULL,
   `language` VARCHAR(2) DEFAULT NULL,
   `image` VARCHAR(255) DEFAULT NULL,
   `order` INT(11) NULL DEFAULT '10000',
@@ -13,9 +13,5 @@ CREATE TABLE IF NOT EXISTS `link` (
   PRIMARY KEY (`id`),
   INDEX `fk_link_domain1_idx` (`domain_id` ASC),
   UNIQUE INDEX `mul_path_domain_id_idx` (`path` ASC, `domain_id` ASC),
-  CONSTRAINT `fk_link_domain1`
-    FOREIGN KEY (`domain_id`)
-    REFERENCES `domain` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
+  CONSTRAINT `fk_link_domain1` FOREIGN KEY (`domain_id`) REFERENCES `domain` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
--- src/infrastructure/persistence/mySQL/sql/storedProcedures/userUpdateOne.sql
+++ src/infrastructure/persistence/mySQL/sql/storedProcedures/userUpdateOne.sql
@@ -1,28 +1,20 @@
 DROP PROCEDURE IF EXISTS user_update;
-
 -- Stored procedure to insert post and tags
 CREATE PROCEDURE user_update(
   IN $USER_ID VARCHAR(40),
   IN $NAME VARCHAR(40),
   IN $EMAIL VARCHAR(40),
-  IN $IMAGE VARCHAR(255),
-  IN $STATEMENT VARCHAR(40),
+  IN $IMAGE TEXT,
+  IN $STATEMENT TEXT,
   IN $LOCATION VARCHAR(40)
-)
-
-BEGIN
-
-  -- Retrieve user
-  UPDATE user
-  SET
-   `name`       = $NAME,
-   `email`      = $EMAIL,
-   `statement`  = $STATEMENT,
-   `image`      = $IMAGE,
-   `location`   = $LOCATION,
-   `updatedAt`  = UNIX_TIMESTAMP()
-  WHERE id      = $USER_ID;
-
-  SELECT $USER_ID AS userId;
-
+) BEGIN -- Retrieve user
+UPDATE user
+SET `name` = $NAME,
+  `email` = $EMAIL,
+  `statement` = $STATEMENT,
+  `image` = $IMAGE,
+  `location` = $LOCATION,
+  `updatedAt` = UNIX_TIMESTAMP()
+WHERE id = $USER_ID;
+SELECT $USER_ID AS userId;
 END
