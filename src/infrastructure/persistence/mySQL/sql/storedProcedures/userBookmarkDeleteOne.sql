91c9940037a0f3e7267096e706b855f627b6e115
Antonio Díaz Correa <antonio.d.correa@gmail.com>
2021-06-30 00:37:30 +0300

    UserBookmark remove fix remove vote

--- src/infrastructure/persistence/mySQL/sql/storedProcedures/userBookmarkDeleteOne.sql
+++ src/infrastructure/persistence/mySQL/sql/storedProcedures/userBookmarkDeleteOne.sql
@@ -1,4 +1,3 @@
-
 DROP PROCEDURE IF EXISTS user_bookmark_delete;
 
 -- Stored procedure to delete bookmark
@@ -7,7 +6,6 @@ CREATE PROCEDURE user_bookmark_delete(
 )
 
 BEGIN
-
 
   SET @link_id = (
     SELECT
@@ -32,11 +30,17 @@ BEGIN
   DELETE FROM bookmark
   WHERE id            = $BOOKMARK_ID;
 
+  -- Remove votes for this bookmark
+  DELETE user_link FROM user_link
+  LEFT JOIN bookmark ON bookmark.link_id = user_link.link_id
+  WHERE bookmark.id IS NULL AND user_link.link_id = @link_id;
+
+  -- Remove link
   DELETE link FROM link
   LEFT JOIN bookmark ON bookmark.link_id = link.id
   WHERE bookmark.id IS NULL AND link.id = @link_id;
 
-    -- Delete domain in case is not used by other link entries
+  -- Delete domain in case is not used by other link entries
   DELETE domain FROM domain
   LEFT JOIN link ON link.domain_id = domain.id
   WHERE link.id IS NULL AND domain.id = @domain_id;
