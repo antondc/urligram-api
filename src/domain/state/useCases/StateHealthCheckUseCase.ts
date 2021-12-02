fd5859c128e793d90789e47eec9c916be19544b8
Antonio Díaz Correa <hello@antoniodiaz.me>
2021-12-02 21:06:11 +0100

    Print email data

--- src/domain/state/useCases/StateHealthCheckUseCase.ts
+++ src/domain/state/useCases/StateHealthCheckUseCase.ts
@@ -1,4 +1,5 @@
 import { IStateRepo } from '@domain/state/repositories/IStateRepo';
+import { EMAIL_HOST, EMAIL_PASSWORD, EMAIL_PORT, EMAIL_TLS, EMAIL_USER } from '@shared/constants/env';
 
 export interface IStateHealthCheckUseCase {
   execute: () => unknown;
@@ -15,7 +16,12 @@ export class StateHealthCheckUseCase {
     const MySQL = await this.stateRepo.healthCheck();
     const Node = process.version;
     const OS = process.platform;
+    const EMAIL_HOST_ = '=> ' + EMAIL_HOST;
+    const EMAIL_PASSWORD_ = '=> ' + EMAIL_PASSWORD;
+    const EMAIL_PORT_ = '=> ' + EMAIL_PORT;
+    const EMAIL_TLS_ = '=> ' + EMAIL_TLS;
+    const EMAIL_USER_ = '=> ' + EMAIL_USER;
 
-    return { MySQL, Node, OS };
+    return { MySQL, Node, OS, EMAIL_HOST_, EMAIL_PASSWORD_, EMAIL_PORT_, EMAIL_TLS_, EMAIL_USER_ };
   }
 }
