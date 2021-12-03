31bb22c25d8de02563dc9e3b990482eaa7d8dc71
Antonio Díaz Correa <hello@antoniodiaz.me>
2021-12-03 22:49:33 +0100

    Clean unused code at state

--- src/domain/state/useCases/StateHealthCheckUseCase.ts
+++ src/domain/state/useCases/StateHealthCheckUseCase.ts
@@ -1,5 +1,4 @@
 import { IStateRepo } from '@domain/state/repositories/IStateRepo';
-import { EMAIL_HOST, EMAIL_PASSWORD, EMAIL_PORT, EMAIL_USER } from '@shared/constants/env';
 
 export interface IStateHealthCheckUseCase {
   execute: () => unknown;
@@ -16,11 +15,7 @@ export class StateHealthCheckUseCase {
     const MySQL = await this.stateRepo.healthCheck();
     const Node = process.version;
     const OS = process.platform;
-    const EMAIL_HOST_ = '=> ' + EMAIL_HOST;
-    const EMAIL_PASSWORD_ = '=> ' + EMAIL_PASSWORD;
-    const EMAIL_PORT_ = '=> ' + EMAIL_PORT;
-    const EMAIL_USER_ = '=> ' + EMAIL_USER;
 
-    return { MySQL, Node, OS, EMAIL_HOST_, EMAIL_PASSWORD_, EMAIL_PORT_, EMAIL_USER_ };
+    return { MySQL, Node, OS };
   }
 }
