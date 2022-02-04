db7651ef463ad6f60540573831f466b97d1b7e15
antondc <hello@antoniodiaz.me>
2022-02-04 14:15:05 +0300

    FileImage ratio

--- src/domain/file/entities/FileImage.ts
+++ src/domain/file/entities/FileImage.ts
@@ -1,3 +1,5 @@
+import Jimp from 'jimp';
+
 import { IFileRepo } from '@domain/file/repositories/IFileRepo';
 import { IImageSaveOneRequest } from '@domain/file/useCases/interfaces/IFileSaveOneRequest';
 import { IImageSaveOneResponse } from '@domain/file/useCases/interfaces/IFileSaveOneResponse';
@@ -27,13 +29,27 @@ export class FileImage extends File {
   }
 
   async fileImageSaveOne(imageSaveOneRequest: IImageSaveOneRequest): Promise<IImageSaveOneResponse> {
+    const ratio = await this.getImageProportions(imageSaveOneRequest.fileUrl);
     const image = await this.fileRepo.fileImageSaveOne(imageSaveOneRequest);
 
-    return image;
+    return {
+      path: image.path,
+      ratio,
+    };
   }
 
   setImageColorProfile = (): void => {
     return null;
+  };
+
+  getImageProportions = async (path: string): Promise<number> => {
+    const image = await Jimp.read(path);
+    const height = image.bitmap.height;
+    const width = image.bitmap.width;
+    const ratio = height / width;
+    const roundedRatio = +ratio.toFixed(2); // https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
+
+    return roundedRatio;
   };
 
   getFormattedImageUrls = ({ imageUrl, sizes = [] }: ImageUrlSplitBySizes): ImageUrlSplitBySizesReturn => {
--- src/domain/file/useCases/interfaces/IFileSaveOneResponse.ts
+++ src/domain/file/useCases/interfaces/IFileSaveOneResponse.ts
@@ -1,3 +1,4 @@
 export type IImageSaveOneResponse = {
   path: string;
+  ratio: number;
 };
