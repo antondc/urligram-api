d221dba5a94592f228b588177266314669819c7b
antondc <hello@antoniodiaz.me>
2022-02-27 12:00:17 +0300

    Disable email service

--- src/shared/services/MailService.ts
+++ src/shared/services/MailService.ts
@@ -18,28 +18,29 @@ export class MailService {
   transporter: Transporter;
 
   constructor(connectionOptions: ConnectionOptions) {
-    this.transporter = nodemailer.createTransport({
-      host: connectionOptions.host,
-      port: connectionOptions.port,
-      auth: {
-        user: connectionOptions.user,
-        pass: connectionOptions.pass,
-      },
-    });
+    // this.transporter = nodemailer.createTransport({
+    //   host: connectionOptions.host,
+    //   port: connectionOptions.port,
+    //   auth: {
+    //     user: connectionOptions.user,
+    //     pass: connectionOptions.pass,
+    //   },
+    // });
   }
 
   sendMail(mailOptions: MailOptions): Promise<{ success: boolean; payload: unknown }> {
-    return this.transporter
-      .sendMail(mailOptions)
-      .then((payload) => {
-        console.log('success');
+    return new Promise((resolve) => resolve({ success: true, payload: '' }));
+    // return this.transporter
+    //   .sendMail(mailOptions)
+    //   .then((payload) => {
+    //     console.log('success');
 
-        return { success: true, payload };
-      })
-      .catch((payload) => {
-        console.log('Email error: ', payload);
+    //     return { success: true, payload };
+    //   })
+    //   .catch((payload) => {
+    //     console.log('Email error: ', payload);
 
-        return { success: false, payload };
-      });
+    //     return { success: false, payload };
+    //   });
   }
 }
