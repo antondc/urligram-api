f49b60da6385288acba97401969c725cf4eacf0d
antondc <hello@antoniodiaz.me>
2022-05-20 13:58:55 +0200

    Send authentication error on prod

--- src/infrastructure/http/middlewares/ErrorHandlerMiddleware.ts
+++ src/infrastructure/http/middlewares/ErrorHandlerMiddleware.ts
@@ -8,12 +8,7 @@ export const ErrorHandlerMiddleware = (err, req, res, next) => {
   const isCustomError = err instanceof BaseError || err instanceof NetWorkError || err instanceof RequestError;
   const isAuthenticationError = err instanceof AuthenticationError;
 
-  if (err && isAuthenticationError)
-    return res
-      .clearCookie('sessionToken', { path: '/' })
-      .status(err.statusCode)
-      .send(LOGGING ? { error: err } : err.message)
-      .end();
+  if (err && isAuthenticationError) return res.clearCookie('sessionToken', { path: '/' }).status(err.statusCode).send({ error: err }).end();
 
   if (err && isCustomError) return res.status(err.statusCode).send({ error: err });
   if (err && LOGGING) return res.status(err.statusCode || 500).send({ error: { message: err?.message, stack: err?.stack } });
