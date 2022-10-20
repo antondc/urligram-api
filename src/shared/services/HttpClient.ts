e11a90ccea10be712f99c8364000048b82f4c87d
antondc <hello@antoniodiaz.me>
2022-10-20 12:36:58 +0200

    HttpClient error

--- src/shared/services/HttpClient.ts
+++ src/shared/services/HttpClient.ts
@@ -3,6 +3,14 @@ import https from 'https';
 
 import { QueryStringWrapper } from './QueryStringWrapper';
 
+type ResponseError = {
+  response: {
+    data: {
+      error: Error;
+    };
+  };
+};
+
 interface Options {
   timeout?: number;
   credentials?: boolean;
@@ -35,7 +43,13 @@ export class HttpClient {
 
         return response.data;
       },
-      (err) => Promise.reject(err)
+      (error: ResponseError) => {
+        const errorContent = error?.response?.data?.error;
+
+        // Returns the data body contained by the response error object instead of the custom native error retrieved by the Error code
+        // Requires backend returning ResponseError object
+        return Promise.reject(errorContent);
+      }
     );
 
     HttpClient.staticInstance = axiosInstance;
